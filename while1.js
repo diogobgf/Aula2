c0637873be9352751e1af618b44bb91474c29c17
diogobgf <116925404+diogobgf@users.noreply.github.com>
2023-08-03 14:30:14 +0000

    while e for

---
+++ while1.js
@@ -0,0 +1,58 @@
+/**let i = 1
+let x = 1
+let r;
+let y = 30
+let z = 68
+
+while (i <= 10){
+    console.log(i);
+        i++;
+}
+
+while (x <= 100){
+    if (x === y || x===z){
+        r = y + z;
+    }
+    x++;
+}
+
+console.log(r);
+
+let p = 1
+let q = 0
+
+while (p <= 50){
+    if (p % 2 == 0){
+        q = p;
+        console.log(q); 
+    }
+    
+    p++
+}
+
+
+let m = 0
+let e = 0
+
+while (m <= 1000){
+    if (m * 2 <= 1000){
+        e = m * 2;
+        console.log(e)
+    }
+    m++
+}
+**/
+
+let g = 2
+let k = 1
+
+while (k <= 100) {
+    
+if (g / k == g && g / 1 == k && g % k == 0){
+    console.log(g + " Não É um Numero Primo");     
+}if (){
+
+}
+ k++
+
+}
