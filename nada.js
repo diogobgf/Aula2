8c9a842c7cf37f3b6f69a02c300dc94ee4aae700
diogobgf <116925404+diogobgf@users.noreply.github.com>
2023-08-29 12:18:03 +0000

    comerço

---
+++ nada.js
@@ -0,0 +1,4 @@
+let readline = require("readline-sync");
+let num = readline.question("Digite um Numero: ");
+let nome = readline.question("Digite um nome: ");
+console.log(num, nome);
--- slidesAulas.js
+++ slidesAulas.js
@@ -1,1 +1,23 @@
-console.log("https://drive.google.com/drive/folders/1DvjSpNOtLFHmaBwSF2Vtq7SHHr3FOWfe");
+//console.log("https://drive.google.com/drive/folders/1DvjSpNOtLFHmaBwSF2Vtq7SHHr3FOWfe");
+
+let readline = require("readline-sync");
+
+let qRodas = readline.question("Digite a Quantidade de Rodas do Veículo: ");
+let peso = readline.question('Digite o Peso do Veículo (valores em Kg): ');
+let qPessoas = readline.question('Digite a Quantidade de Pessoas que o Veículo Suporta: ');
+
+if ((qRodas == 2) || (qRodas == 3)) {
+    console.log("Veículo Categoria A");
+}
+else if  ((qRodas == 4 ) && (qPessoas <= 8) && (peso <= 3500)) {
+  console.log("Veículo Categoria B");
+}
+else if ((qRodas >= 4) && (peso > 3500) && (peso < 6000)) {
+    console.log("Veículo Categoria C");
+}
+else if ((qRodas >= 4) && (qPessoas > 8)) {
+    console.log("Veículo Categoria D");
+}
+else if ((qRodas >= 4) && (peso > 6000)) {
+    console.log("Veículo Categoria E");
+}
